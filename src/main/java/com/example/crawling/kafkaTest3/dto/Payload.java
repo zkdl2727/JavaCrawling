d0df858d1ce389d3f195509c9d30937746635b56
김성수 <99383331+zkdl2727@users.noreply.github.com>
2024-01-21 16:43:56 +0900

    add : 사용자 정보 dto 구현.

--- src/main/java/com/example/crawling/kafkaTest3/dto/Payload.java
+++ src/main/java/com/example/crawling/kafkaTest3/dto/Payload.java
@@ -1,27 +1,28 @@
 package com.example.crawling.kafkaTest3.dto;
 
-import org.springframework.boot.json.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Payload {
 
+public class Payload {
     private String userEmail;
     private String message;
 
-    public Payload(String userEmail, String message) {
-        this.userEmail = userEmail;
-        this.message = message;
-    }
+    // 생성자, getter, setter 생략...
 
     public static Payload fromJson(String json) throws JsonProcessingException {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(json, Payload.class);
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, Payload.class);
     }
 
     public String toJson() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(this);
+    }
+
+    public Payload(String userEmail, String message) {
+        this.userEmail = userEmail;
+        this.message = message;
     }
 
     public String getUserEmail() {
